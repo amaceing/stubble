067d8944688af0cc65353825f8499145d404c4f5
Anthony Mace <amace93@gmail.com>
2016-08-20 23:23:29 -0700

    empty project with 5 view controllers tied to tab vc

---
+++ Stubble/FeedViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FeedViewController.swift
+//  Stubble
+//
+//  Created by Anthony Mace on 8/20/16.
+//  Copyright © 2016 Mace. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
