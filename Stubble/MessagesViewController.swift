6160bd71ae46e841efba7625e0c552bcd8a3f026
Anthony Mace <amace93@gmail.com>
2016-08-21 00:13:06 -0700

    set titles in nibs

--- Stubble/MessagesViewController.swift
+++ Stubble/MessagesViewController.swift
@@ -9,10 +9,9 @@
 import UIKit
 
 class MessagesViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
--- Stubble/TicketManagerViewController.swift
+++ Stubble/TicketManagerViewController.swift
@@ -9,10 +9,8 @@
 import UIKit
 
 class TicketManagerViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
