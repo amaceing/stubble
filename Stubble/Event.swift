3d3216810fee69812fac27499ab4ecc8f1e16037
Anthony Mace <amace93@gmail.com>
2016-08-21 14:41:19 -0700

    file for Event model, stored properties and initializer

---
+++ Stubble/Event.swift
@@ -0,0 +1,36 @@
+//
+//  Event.swift
+//  Stubble
+//
+//  Created by Anthony Mace on 8/21/16.
+//  Copyright © 2016 Mace. All rights reserved.
+//
+
+import Foundation
+
+enum EventType {
+    case Basketball
+    case Baseball
+    case Football
+    case Soccer
+    case Concert
+    case Play
+}
+
+class Event: NSObject {
+    private var title: String
+    private var eventType: EventType
+    private var numberOfTickets: Int
+    private var dateAndTime: NSDate
+    private var askingPrice: Int
+    private var eventDescription: String
+    
+    init(title: String, eventType: EventType, numberOfTickets: Int, dateAndtime: NSDate, askingPrice: Int, eventDesc: String) {
+        self.title = title
+        self.eventType = eventType
+        self.numberOfTickets = numberOfTickets
+        self.dateAndTime = dateAndtime
+        self.askingPrice = askingPrice
+        self.eventDescription = eventDesc
+    }
+}
