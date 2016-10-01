731e22e7e9722bf48d32290948b6aa8ab523bcd5
Anthony Mace <amace93@gmail.com>
2016-09-30 21:55:45 -0700

    changed DB.read to be .value to it doesn't only fire once, changed tv cells and changed tv to use sections

--- Stubble/FeedViewController.swift
+++ Stubble/FeedViewController.swift
@@ -20,15 +20,26 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.dataSource = FirebaseTableViewDataSource(ref: ref, cellReuseIdentifier: "events", view: self.feed)
-        self.feed.dataSource = self.dataSource
-        self.dataSource.populateCell {(cell: UITableViewCell, obj: NSObject) -> Void in
-            let snap = obj as! FIRDataSnapshot
-            let event = Event(snap)
-            
-            cell.textLabel?.text = event.title
-        }
+        // Do any additional setup after loading the view, typically from a nib.self.dataSource = FirebaseTableViewDataSource(ref: ref, cellReuseIdentifier: "events", view: self.feed)
+        self.feed.dataSource = self
+        self.feed.delegate = self
+        DB.read(collection: "events", className: "Event", callback: { (data:[AnyObject]) -> Void in
+            if (self.events.count == 0) {
+                self.events = data as! [Event]
+            } else {
+                let newEvent = data[data.count - 1] as! Event
+                self.events.append(newEvent)
+            }
+            self.feed.reloadData()
+        })
+        
+//        self.ref.observe(.value, with: {(snapshot) -> Void in
+//            for event in snapshot.children {
+//                let eventToAdd = Event(event as! FIRDataSnapshot)
+//                self.events.append(eventToAdd)
+//            }
+//            self.feed.reloadData()
+//        })
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -44,27 +55,36 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func numberOfSections(in tableView: UITableView) -> Int {
         //implement later
-        return 1
+        return self.events.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //implement later
-        let count = self.events.count
-        return count
+        return 1
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         //implement later
-        return 1
+        return 134
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 8
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1.0)
+        return view
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //implement later
         let cell = tableView.dequeueReusableCell(withIdentifier: "event", for: indexPath) as! EventTableViewCell
-        let event = events[indexPath.row]
+        let event = events[indexPath.section]
         cell.titleOfEvent?.text = event.title
         cell.eventLocation.text = event.eventDescription
-        cell.askingPriceOfTicket.text = String(event.askingPrice)
+        cell.askingPriceOfTicket.text = "$" + String(event.askingPrice)
         let dateComponents = getDateAndTime(from: event)
         cell.dateOfEvent.text = dateComponents.0
         cell.timeOfEvent.text = dateComponents.1
