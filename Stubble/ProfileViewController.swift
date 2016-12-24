0558363bf61d5a926f6bfec131261d8a74966ee8
Anthony Mace <amace93@gmail.com>
2016-12-23 21:40:31 -0700

    layout for basic profile, cropping image to circle

--- Stubble/ProfileViewController.swift
+++ Stubble/ProfileViewController.swift
@@ -8,16 +8,30 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameInput: UITextField!
+    @IBOutlet weak var emailInput: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.cropProfileImageToCircle()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func cropProfileImageToCircle() {
+        self.profileImage.layer.borderWidth = 1
+        self.profileImage.layer.masksToBounds = false
+        self.profileImage.layer.borderColor = UIColor.gray.cgColor
+        self.profileImage.layer.backgroundColor = UIColor.gray.cgColor
+        self.profileImage.layer.cornerRadius = self.profileImage.frame.height/2
+        self.profileImage.clipsToBounds = true
     }
 
 
