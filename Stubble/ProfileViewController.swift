34761e42df8fbfdda5d0d6869501cd6d4eb18c9b
Anthony Mace <amace93@gmail.com>
2016-12-24 13:21:17 -0700

    functionality for picking user photo

--- Stubble/ProfileViewController.swift
+++ Stubble/ProfileViewController.swift
@@ -7,17 +7,23 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
-class ProfileViewController: UIViewController, UIImagePickerControllerDelegate {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var nameInput: UITextField!
     @IBOutlet weak var emailInput: UITextField!
+    let picker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.cropProfileImageToCircle()
+        picker.delegate = self
+        let user = FIRAuth.auth()?.currentUser
+        print(user!)
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,13 +34,20 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate {
     func cropProfileImageToCircle() {
         self.profileImage.layer.borderWidth = 1
         self.profileImage.layer.masksToBounds = false
-        self.profileImage.layer.borderColor = UIColor.gray.cgColor
-        self.profileImage.layer.backgroundColor = UIColor.gray.cgColor
-        var radius = self.profileImage.frame.size.width/2
+        self.profileImage.layer.borderColor = UIColor.clear.cgColor
         self.profileImage.layer.cornerRadius = 100
         self.profileImage.clipsToBounds = true
     }
 
+    @IBAction func changeImage(_ sender: UIButton) {
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        self.profileImage.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        picker.dismiss(animated: true, completion: nil)
+    }
 
 }
 
